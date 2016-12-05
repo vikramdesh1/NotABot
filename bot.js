5221cf2a2b3850e6a1840c162d91622873e6d11c
Vikram Deshmukh <vikram@Vikrams-MacBook-Pro.local>
2016-12-04 22:31:14 -0500

    testing node-rest-client

--- bot.js
+++ bot.js
@@ -28,6 +28,9 @@ function respond() {
     this.res.writeHead(200);
     postMessage(accessToken);
     postMessage(typeof accessToken);
+    var url = "https://api.groupme.com/v3/groups?TOKEN=" + accessToken;
+    postMessage(url);
+    postMessage(typeof url);
     this.res.end();
     testGet();
   } else {
