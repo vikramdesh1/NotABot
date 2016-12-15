1932c8182de0a75d1c6e58d4f32927bf0ff0f024
Vikram Deshmukh <vikramdesh1@live.com>
2016-12-14 21:04:18 -0500

    handled null message in randommessage

--- js/bot.js
+++ js/bot.js
@@ -144,7 +144,12 @@ function getRandomMessage(userName, whenDone) {
       var index = utilities.getRandomInt(0, messages.length - 1);
       var message = messages[index];
       var timestamp = new Date(message.created_at * 1000);
-      var text = "Message #" + (index + 1) + " - \n" + message.name + " (" + (timestamp.getMonth() + 1) + "/" + timestamp.getDate() + "/" + timestamp.getFullYear() + ") : " + message.text;
+      var text = "Message #" + (index + 1) + " - \n" + message.name + " (" + (timestamp.getMonth() + 1) + "/" + timestamp.getDate() + "/" + timestamp.getFullYear() + ") : ";
+      if(message.text == null) {
+        text += "(no message)";
+      } else {
+        text += message.text;
+      }
       var attachments = message.attachments;
       whenDone(text, attachments);
     } else {
